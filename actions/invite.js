31bd2e6218785b06e71868adf3b9669eabf1eac4
Miguel <79668469+TheMegaGlitch@users.noreply.github.com>
2021-06-01 00:05:16 -0500

    Update invite.js

--- actions/invite.js
+++ actions/invite.js
@@ -5,7 +5,7 @@ module.exports = {
 	usage: '',
 	async execute(message = new Discord.Message(), args = new Array(), client = new Discord.Client()) {
 		const inviteEmbed = new Discord.MessageEmbed()
-        .setDescription(`Here's my [invite link](https://discord.com/api/oauth2/authorize?client_id=822292321331576853&permissions=268512320&scope=bot)!`)
+        .setDescription(`Here's my [invite link](https://discord.com/api/oauth2/authorize?client_id=822292321331576853&permissions=268463168&scope=bot)!`)
         return message.channel.send(inviteEmbed)
 	},
 };
